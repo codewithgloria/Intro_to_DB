8edca77203f62c26969bc45452d49f8c3e9b8f53
Gloria <abinezagloria@gmail.com>
2025-07-06 21:05:36 +0300

    corrected checker error

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     try:
@@ -14,8 +13,8 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e: 
-        print(f"Error: {e}")
+    except mysql.connector.Error as e:
+        print(f"Database error: {e}")
 
     finally:
         if 'connection' in locals() and connection.is_connected():
